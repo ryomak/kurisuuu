b6dfb66a33ea69cd6cfb55a69826fb8bf7ee23b5
ryomak <ryooomaaa0413@outlook.com>
2018-04-07 20:12:32 +0900

    fix

--- client/src/components/movie/movie.vue
+++ client/src/components/movie/movie.vue
@@ -1,14 +1,19 @@
 <template>
-    <div>
-        <div v-for="item in list" >
-            <div>
-                {{item.name}}
-                <video :name="item.name" preload="auto" controls :poster="item.imgPath">
+    <div class="container">
+        <section class="card" v-for="item in list">
+                <video class="card-img" :name="item.name" preload="auto" controls :poster="item.imgPath">
                     <source :src="item.path" type="video/mp4"/>
                     <p>ご利用のブラウザはvideo タグによる動画の再生に対応していません。</p>
                 </video>
+            <div class="card-content">
+                <h1 class="card-title">{{item.name}}</h1>
+                <img :src="/static/">
             </div>
-        </div>
+            <div class="card-link">
+                <a href="http://webcreatorbox.com/about">About</a>
+                <a href="http://webcreatorbox.com/">Website</a>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -28,89 +33,60 @@
                 })
             });
         }
+        prev(){}
+        next(){
+            console.log("next");
+        }
+        getIconPath(name:string){
+            return "/static/"
+        }
     };
 </script>
 
-<style scoped>
-    .stack-wrapper{
-        margin: 0 auto;
-        position: relative;
-        z-index: 1000;
-        width: 320px;
-        height: 320px;
-        padding: 0;
-        list-style: none;
-        pointer-events: none;
+<style scoped lang="scss">
+    .container{
+        display: flex;
+        flex-flow: column wrap;
+        justify-content:center;
     }
-    .controls{
-        position: relative;
-        width: 200px;
-        text-align: center;
-        display:flex;/*Flex布局*/
-        display: -webkit-flex; /* Safari */
-        justify-content:space-around;
-        margin: 0 auto;
-        margin-top: 50px
-    }
-    .controls .button {
-        border: none;
-        background: none;
-        position: relative;
-        display: inline-block;
-        cursor: pointer;
-        font-size: 16px;
-        width: 50px;
-        height: 50px;
-        z-index: 100;
-        -webkit-tap-highlight-color: rgba(0,0,0,0);
-        border-radius: 50%;
+    .card {
+        width: 350px;
         background: #fff;
+        border-radius: 5px;
+        box-shadow: 0 2px 5px #ccc;
+        display: inline-flex;
     }
-    .button .next{
-        display: inline-block;
-        width: 10px;
-        height:5px;
-        background: rgb(129,212,125);
-        line-height: 0;
-        font-size:0;
-        vertical-align: middle;
-        -webkit-transform: rotate(45deg);
-        left: -5px;
-        top: 2px;
-        position: relative;
+    .card-img {
+        border-radius: 5px 5px 0 0;
+        max-width: 100%;
+        height: auto;
     }
-    .button .next:after{
-        content:'/';
-        display:block;
-        width: 20px;
-        height:5px;
-        background: rgb(129,212,125);
-        -webkit-transform: rotate(-90deg) translateY(-50%) translateX(50%);
+    .card-content {
+        padding: 20px;
     }
-    .button .prev{
-        display: inline-block;
-        width: 20px;
-        height:5px;
-        background: rgb(230,104,104);
-        line-height: 0;
-        font-size:0;
-        vertical-align: middle;
-        -webkit-transform: rotate(45deg);
+    .card-title {
+        font-size: 20px;
+        margin-bottom: 20px;
+        text-align: center;
+        color: #333;
     }
-    .button .prev:after{
-        content:'/';
-        display:block;
-        width: 20px;
-        height:5px;
-        background: rgb(230,104,104);
-        -webkit-transform: rotate(-90deg);
+    .card-text {
+        color: #777;
+        font-size: 14px;
+        line-height: 1.5;
     }
-    .controls .text-hidden {
-        position: absolute;
-        overflow: hidden;
-        width: 0;
-        height: 0;
-        color: transparent;
-        display: block;
+    .card-link {
+        text-align: center;
+        border-top: 1px solid #eee;
+        padding: 20px;
     }
+    .card-link a {
+        text-decoration: none;
+        color: #0bd;
+        margin: 0 10px;
+    }
+    .card-link a:hover {
+        color: #0090aa;
+    }
+
 </style>
